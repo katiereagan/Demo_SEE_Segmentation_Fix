3655caacd39ccc66e94469680a9b040df4cb606a
Katie Reagan <134813731+katiereagan@users.noreply.github.com>
2023-07-10 22:43:56 -0400

    Updated with while loop, file upload/save via user input

--- test.py
+++ test.py
@@ -5,8 +5,10 @@ from collections import Counter
 
 def main():
 
+    input_image_path = input("Enter the pathway to your image: ")
+
     # INSERT image pathway: Test image:
-    image = cv2.imread('testimage.jpg')
+    image = cv2.imread(input_image_path)
 
     # Convert the image into an array:
     numpy_array = np.array(image)
@@ -15,7 +17,7 @@ def main():
     # (n, 3), with one column, n rows, and 3 terms/tuple:
     reshaped_array = numpy_array.reshape(-1, 3)
 
-    # tuple(row) converts each row of the reshaped_array into a tuple
+    # tuple(row) converts each row of the reshaped_array into a tuple.
     # 'for row' assigns each element in reshaped_array to 'row'.
     # This converts the reshaped array into a list of tuples:
     tuple_list = [tuple(row) for row in reshaped_array]
@@ -39,7 +41,7 @@ def main():
     # Gives the remaining tuples with the lower frequencies, since these should be
     # those not intended to exist within the segmented image:
     lfreq_tuples = [tpl for tpl in tuple_counts if tpl not in dict(hfreq_tuples)]
-    if lfreq_tuples:
+    while lfreq_tuples:
 
         print("\nUnintended segmentation colors:")
         for tpl in lfreq_tuples:
@@ -50,6 +52,9 @@ def main():
 
             # Prints the tuple and their frequencies:
             print(f"\nTuple: {tpl}, Frequency: {count}")
+
+            # Saves this value to append at the end if no surrounding intended tuples are found.
+            orig_tpl = tpl
 
             # idx is a list that contains the indices tuple_list items that are equal to tpl:
             idx = [i for i, t in enumerate(tuple_list) if t == tpl]
@@ -71,15 +76,13 @@ def main():
                 print(f"Row Index: {row_idx}")
                 print(f"Column Index: {col_idx}")
                 retrieved_tuple = numpy_array[row_idx, col_idx]
-                # flat_index = np.ravel_multi_index((row_idx, col_idx), numpy_array.shape[:-1])
-                # print(f"Index: {flat_index}")
 
-                # Creates an iterative loop from row_idx-1 to, but excluding, row_idx+2:
-                for r in range(row_idx - 1, row_idx + 2):
+                # Creates an iterative loop within a certain index radius:
+                for r in range(row_idx - 3, row_idx + 4):
 
                     # This is indented since we want the index to be within the
                     # correct rows and columns, creating a 3x3 search area around a tuple:
-                    for c in range(col_idx - 1, col_idx + 2):
+                    for c in range(col_idx - 5, col_idx + 6):
 
                         # Note that shape[0] gives the 0-index element of tuples, which gives the rows,
                         # and shape[1] gives the 1-index element of the tuples, which gives the columns:
@@ -94,63 +97,61 @@ def main():
                 # Prints a list of surrounding tuples under each iteration:
                 print(surrounding_tuples)
 
-                obj_list = [("error")]
+                obj_list = [("No correct surrounding colors")]
                 count_list = [0]
 
-                # Iterate over the indexes of hfreq_tuples
+                # Iterate over the indexes of hfreq_tuples:
                 for i, (tpl, _) in enumerate(hfreq_tuples):
 
-                    # Initialize a counter for the current index
+                    # Initialize a counter for the current index:
                     counter = Counter()
 
-                    # Iterate over the surrounding tuples
+                    # Iterate over the surrounding tuples:
                     for surrounding_tuple in surrounding_tuples:
                         if surrounding_tuple == tpl:
                             counter[surrounding_tuple] += 1
 
-                    # Print the counts for the current index
+                    # Print the counts for the current index:
                     for obj, count in counter.items():
                         print(f"Frequency of {obj}: {count}")
+
+                        # obj_list records what type of hfreq_tuples are present.
                         obj_list.append(obj)
+
+                        # count_list records how many of each hfreq_tuples are present.
                         count_list.append(count)
-                print(obj_list, count_list)
-                # try saving obj to obj_list and count to count_list. Find largest item in count list, then determine its index
-                # its corresponding object in the obj_list should give with obj is most frequent.
+
+                # Find largest item in count list, then determine its index.
+                # The corresponding object in the obj_list should give with obj is most frequent.
+
+                # Gives the frequency of the most common hfreq_tuple:
                 max_count = max(count_list)
-                print(max_count)
+
+                # Gives the index of the most common list:
                 max_index = count_list.index(max_count)
-                print(max_index)
+
+                # max_obj records which hfreq_tuple is most common.
                 max_obj = obj_list[max_index]
-                print(max_obj)
-                # print(numpy_array[flat_index])
-                print(retrieved_tuple)
-                if max_obj != 'error':
+                if max_obj != 'No correct surrounding colors':
                     numpy_array[row_idx, col_idx] = max_obj
+                    print(f"New color: {numpy_array[row_idx, col_idx]}")
+                    # Can un-comment below to save a recursively updating version of the image:
+                    #output_image = np.uint8(numpy_array)
+                    #output_filename = f'INSERT_FILEPATH'
+                    #cv2.imwrite(output_filename, output_image)
                 else:
-                    print("No correct surrounding colors")
-                print(numpy_array[row_idx, col_idx])
-        print(numpy_array)
+                    # The original tuple is appended to the end of the list so that it can be cycled through again at the end:
+                    lfreq_tuples.append(orig_tpl)
 
-        # Convert the modified numpy_array back to an image
-        output_image = np.uint8(numpy_array)
+    output_image = np.uint8(numpy_array)
 
-        # INSERT file name: Save the image to a file
-        output_filename = 'output_image.jpg'  # Choose a filename and extension for the output image
-        cv2.imwrite(output_filename, output_image)
+    # User inputs the pathway to their file:
+    output_image_path = input("Enter the pathway to your new image: ")
+    cv2.imwrite(output_image_path, output_image)
 
-        # Display a message indicating the image has been saved
-        print(f"The modified image has been saved as {output_filename}")
-
-        # Eventually make it so that the loop runs, re-evaluating unintended
-        # tuples until there are none left.
-
-        # Makes things look grainy...instead make it easier for them to manually choose
-        # what color value to switch it to?
-
-        # Add way to prompt user for upload file pathname and saved file name
-
-    else:
-        print("\nNo unintended segmentation colors.")
+    # Display a message indicating the image has been saved:
+    print(f"The modified image has been saved to {output_image_path}")
+    print("\nNo unintended segmentation colors remain.")
 
 if __name__ == '__main__':
     main()
